9cb5b30dfbf72f21df4ae50abc766a75f8b10adc
francis <francisogonna423@gmail.com>
2025-06-18 09:20:55 +0100

    github page

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/<repo-name>/', // Replace <repo-name> with your GitHub repository name
+  base: '/brixium/', // Replace <repo-name> with your GitHub repository name
   server: {
     port: 3000
   }
